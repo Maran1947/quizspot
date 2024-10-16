15aad697c9f38acb2290caa8940d51e8ffb93d6c
Abhishek Maran <abhishekmaran1947@gmail.com>
2024-10-16 14:57:47 +0530

    modified get all quizzes api to support filtering by type

--- src/app/api/quiz/all/route.ts
+++ src/app/api/quiz/all/route.ts
@@ -1,16 +1,53 @@
 import { auth } from '@/auth'
+import { IQuizAttemptsGroup, IQuizResult } from '@/interfaces/quiz'
 import { prisma } from '@/prismaClient'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const filterBy = req.nextUrl.searchParams.get('filterBy')
   try {
     const session = await auth()
-    const user = await prisma.user.findUnique({ where: { email: session?.user?.email as string } })
-    
+    const user = await prisma.user.findUnique({
+      where: { email: session?.user?.email as string }
+    })
+
     if (!user) {
-      return NextResponse.json({ success: false, error: 'Unauthorized' }, { status: 401 })
+      return NextResponse.json(
+        { success: false, error: 'Unauthorized' },
+        { status: 401 }
+      )
     }
-    const quizzes = await prisma.quiz.findMany({ where: { userId: user.id } })
+    let quizzes = []
+    if (filterBy === 'attempted') {
+      const attempts = await prisma.attempt.findMany({
+        where: { userId: user.id },
+        include: { quiz: true }
+      })
+      const groupedAttempts = attempts.reduce((acc: { [quizId: string]: IQuizAttemptsGroup  }, attempt) => {
+        const quizId = attempt.quizId
+
+        if (!acc[quizId]) {
+          acc[quizId] = {
+            quiz: attempt.quiz,
+            attempts: []
+          }
+        }
+
+        acc[quizId].attempts.push({
+          id: attempt.id,
+          createdAt: attempt.createdAt,
+          quiz: attempt.quiz,
+          quizId: attempt.quizId,
+          result: attempt.result ? (attempt.result as unknown as IQuizResult) : null
+        })
+
+        return acc
+      }, {})
+
+      quizzes = Object.values(groupedAttempts)
+    } else {
+      quizzes = await prisma.quiz.findMany({ where: { userId: user.id } })
+    }
 
     return NextResponse.json({ success: true, quizzes }, { status: 200 })
   } catch (error) {
